fd2bf4c2e48e34be57fde3a25d3386a312939490
Laureano Piotti <laureanopiotti@gmail.com>
2018-08-30 02:53:55 -0300

    terminada clase 1 oop

---
+++ OOP/clase1/prueba.php
@@ -0,0 +1,28 @@
+<?php 
+
+require('usuario.php');
+
+$lalo = new Usuario('lalo','lalo@gmail.com','1234');
+
+$lalo2 = new Usuario('lalo2','lalo2@gmail.com','1234');
+
+
+$lalo->saludar();
+
+
+echo '<br>';
+
+echo $lalo->getNombre();
+
+$lalo->setNombre('Laloncio');
+
+echo $lalo->getNombre();
+
+echo $lalo->getPassword();
+
+echo '<br>';
+
+$lalo->setPassword('123');
+
+echo $lalo->getPassword();
+ ?>
